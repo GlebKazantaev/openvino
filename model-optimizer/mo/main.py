cffda1c896624a80bf33524c5df5ea33933c3f6d
Gleb Kazantaev <gleb.kazantaev@intel.com>
2020-12-10 15:38:03 +0300

    True KSO ON

--- model-optimizer/mo/main.py
+++ model-optimizer/mo/main.py
@@ -251,13 +251,13 @@ def emit_ir(graph: Graph, argv: argparse.Namespace):
         output_dir = argv.output_dir if argv.output_dir != '.' else os.getcwd()
 
         orig_model_name = os.path.join(output_dir, argv.model_name)
-        from openvino.inference_engine import IECore
-        from openvino.inference_engine.offline_api import ApplyMOCTransformations, ApplyMOCTransformationsCPU
-        ie = IECore()
-        net = ie.read_network(model=orig_model_name + ".xml", weights=orig_model_name + ".bin")
+        # from openvino.inference_engine import IECore
+        # from openvino.inference_engine.offline_api import ApplyMOCTransformations, ApplyMOCTransformationsCPU
+        # ie = IECore()
+        # net = ie.read_network(model=orig_model_name + ".xml", weights=orig_model_name + ".bin")
         # ApplyMOCTransformations(net, True)
-        ApplyMOCTransformationsCPU(net)
-        net.serialize(orig_model_name + ".xml", orig_model_name + ".bin")
+        # ApplyMOCTransformationsCPU(net)
+        # net.serialize(orig_model_name + ".xml", orig_model_name + ".bin")
 
         print('\n[ SUCCESS ] Serialize-Read-Serialize')
         print('[ SUCCESS ] Generated IR version {} model.'.format(get_ir_version(argv)))
